03f6c8fa711c3c7d11a99acaaf690a5d5dce08c3
NevilleDNZ <NevilleD.a68c@sgr-a.net>
2024-04-10 18:38:23 +1000

    Add fedora39-x86_64 adn automate actions_runner setup

--- .github/bin/runner_mgr.sh
+++ .github/bin/runner_mgr.sh
@@ -1,48 +1,63 @@
 #!/bin/bash
 
-def_SHR_REPO="NevilleDNZ-downstream/algol68_autopkg-downstream"
+def_SHR_REPO_NAME="NevilleDNZ-downstream/algol68_autopkg-downstream"
+
+OPT_read= # was: -e, but this may not work in ssh??
+UNTAR_DIR=untar
 
 SHR_AR_ROOT=actions-runner
 mkdir -p "$SHR_AR_ROOT"
 
-get_SHR_REPO(){
-    ( cd "$SHR_AR_ROOT"; ls -ltrd */* )
-    read def_SHR_REPO <<< $(echo $def_SHR_REPO; ls -td */* | tail -1)
-    read -e -p "SHR_REPO [ $def_SHR_REPO ]: " SHR_REPO
-    SHR_REPO=${SHR_REPO:-$def_SHR_REPO}
-    SHR_REPO_URL="https://github.com/$SHR_REPO"
-    SHR_URL_TOK_NEW="$SHR_REPO_URL/settings/actions/runners/new"
+get_SHR_REPO_NAME(){
+    case "$1" in
+        ("")
+            ( cd "$SHR_AR_ROOT"; ls -ltrd */* )
+            read def_SHR_REPO_NAME <<< $(echo $def_SHR_REPO_NAME; ls -td */* | tail -1)
+            PROMPTREAD "SHR_REPO_NAME [ $def_SHR_REPO_NAME ]: " SHR_REPO_NAME
+        ;;
+        (*)
+            SHR_REPO_NAME="$1"
+        ;;
+    esac
+    SHR_REPO_NAME=${SHR_REPO_NAME:-$def_SHR_REPO_NAME}
+    SHR_REPO_NAME_URL="https://github.com/$SHR_REPO_NAME"
+    SHR_URL_TOK_NEW="$SHR_REPO_NAME_URL/settings/actions/runners/new"
 }
 
 ignore_depr(){
    sed 's/\/run.sh//; /_depr$/d'
 }
 
-get_default_SHR_NAME(){
+get_default_SHR_RUNNER_NAME(){
    ( cd $SHR_AR_DIR && ( ls -t */run.* 2> /dev/null | ignore_depr ; echo "$(hostname)" ) | head -1)
 }
 
-get_SHR_NAME(){
-    case "$#" in
-        (1)
+get_SHR_RUNNER_NAME(){
+    case "$1" in
+        (""|*"*"*)
             (
                  cd $SHR_AR_ROOT &&
                    ls -tr */*/run.* 2> /dev/null | ignore_depr
-                   get_default_SHR_NAME
+                   get_default_SHR_RUNNER_NAME
             ) | sort -u
-	        def_SHR_NAME=`get_default_SHR_NAME`
-            read -e -p "SHR_NAME [ $def_SHR_NAME ]: " SHR_NAME
-            SHR_NAME=${SHR_NAME:-$def_SHR_NAME}
+	        def_SHR_RUNNER_NAME=`get_default_SHR_RUNNER_NAME`
+            PROMPTREAD "SHR_RUNNER_NAME [ $def_SHR_RUNNER_NAME ]: " SHR_RUNNER_NAME
+            SHR_RUNNER_NAME=${SHR_RUNNER_NAME:-$def_SHR_RUNNER_NAME}
         ;;
-        (2) SHR_NAME="$2" ;;
-        (*) echo "Huh?";;
+        (-) SHR_RUNNER_NAME="$(hostname)";;
+        (*) SHR_RUNNER_NAME="$1";;
     esac
-    SHR_AR_DIR="$SHR_AR_ROOT/$SHR_REPO/$SHR_NAME"
+    SHR_AR_DIR="$SHR_AR_ROOT/$SHR_REPO_NAME/$SHR_RUNNER_NAME"
 }
 
 get_TOKEN(){
-    echo "$SHR_URL_TOK_NEW"
-    read -e -p "Token: " SHR_TOKEN
+    case "$1" in
+        ("")
+            echo "$SHR_URL_TOK_NEW"
+            PROMPTREAD "Token: " SHR_TOKEN
+        ;;
+        (*)SHR_TOKEN="$1";;
+    esac
 }
 
 RAISE(){
@@ -59,6 +74,12 @@ TRACE(){
     "$@"
 }
 
+PROMPTREAD(){
+    prompt="$1"; shift
+    # echo -n "PROMPT: $prompt" 1>&2
+    read $OPT_read -p "$prompt" "$@" 1>&2
+}
+
 TRACE=TRACE
 ASSERT=ASSERT
 WATCH=WATCH
@@ -71,10 +92,30 @@ case "$(uname -m)" in
 esac
 
 V=2.314.1
-AR_TAR=actions-runner-linux-$MACH-$V.tar.gz 
+V=2.315.0
+# actions-runner-linux-arm-2.315.0.tar.gz
+# actions-runner-linux-x64-2.315.0.tar.gz314159
+# actions-runner-linux-arm64-2.315.0.tar.gz
+# actions-runner-osx-x64-2.315.0.tar.gz
+# actions-runner-osx-arm64-2.315.0.tar.gz
+# actions-runner-win-x64-2.315.0.zip
+# actions-runner-win-arm64-2.315.0.zip
+AR_TAR=actions-runner-linux-$MACH-$V.tar.gz
+# echo AR_TAR=$AR_TAR
 
-case "$1" in
-    (download)
+cmd="$1"; shift
+case "$cmd" in
+    (help) # cmd
+        grep "^ *([a-z_]*) # cmd" "$0"
+        exit
+    ;;
+    (test) # cmd
+        get_SHR_REPO_NAME "$1"
+        get_SHR_RUNNER_NAME "$2"
+        get_TOKEN "$3"
+        echo SHR_REPO_NAME=$SHR_REPO_NAME SHR_RUNNER_NAME=$SHR_RUNNER_NAME SHR_TOKEN=$SHR_TOKEN
+    ;;
+    (download) # cmd
         ## Download
         # Create a folder
             # $ASSERT mkdir actions-runner && cd actions-runner
@@ -83,60 +124,84 @@ case "$1" in
 # curl -o actions-runner-linux-arm-2.314.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.314.1/actions-runner-linux-arm-2.314.1.tar.gz
 # curl -o actions-runner-linux-arm64-2.314.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.314.1/actions-runner-linux-arm64-2.314.1.tar.gz
 
-        curl -o $AR_TAR -L https://github.com/actions/runner/releases/download/v$V/$AR_TAR
-     
+        [ ! -f "$AR_TAR" ] && # may root cause a problem if previously curl failed!! :-/
+            ASSERT curl -o $AR_TAR -L https://github.com/actions/runner/releases/download/v$V/$AR_TAR
+
 # Optional: Validate the hash
 # $ASSERT echo "6c726a118bbe02cd32e222f890e1e476567bf299353a96886ba75b423c1137b5  actions-runner-linux-x64-2.314.1.tar.gz" | shasum -a 256 -c
     ;;
-    (configure)
-        get_SHR_REPO "$@"
-        get_SHR_NAME "$@"
+    (installdependencies) # cmd
+        if mkdir -p $UNTAR_DIR && cd $UNTAR_DIR; then
+            $ASSERT tar -xzf $OLDPWD/$AR_TAR # actions-runner-linux-x64-2.314.1.tar.gz
+            set -x
+            $ASSERT sudo bin/installdependencies.sh
+            set +x
+            cd -
+        fi
+    ;;
+    (configure) # cmd
+        get_SHR_REPO_NAME "$1"
+        get_SHR_RUNNER_NAME "$2"
         set +x
-        if mkdir -p $SHR_AR_DIR && cd $SHR_AR_DIR; then
-            get_TOKEN
+        if mkdir -p "$SHR_AR_DIR" && cd "$SHR_AR_DIR"; then
+            get_TOKEN "$3"
             echo "$SHR_TOKEN" > TOKEN.txt
         # Extract the installer
-            $ASSERT tar xzf ../../../../$AR_TAR # actions-runner-linux-x64-2.314.1.tar.gz
+            $ASSERT tar -xzf ../../../../$AR_TAR # actions-runner-linux-x64-2.314.1.tar.gz
         # Configure
         # Create the runner and start the configuration experience
 	# ToDo: ID_LIKE fedora, centos, rhel etc
-        SHR_LABELS="$SHR_NAME,$( . /etc/os-release; echo $ID$VERSION_ID-`uname -m`,$ID$VERSION_ID,$ID,`uname -m`,$ID$VERSION_ID-$MACH,$MACH;)"
-        echo HINT: name: $SHR_NAME - SHR_TOKEN: $SHR_TOKEN - SHR_LABELS: $SHR_LABELS
-            $ASSERT ./config.sh --name "$SHR_NAME" --unattended --labels "$SHR_LABELS" --replace Y --url $SHR_REPO_URL --token $SHR_TOKEN
+        SHR_LABELS="$SHR_RUNNER_NAME,$( . /etc/os-release; echo $ID$VERSION_ID-`uname -m`,$ID$VERSION_ID,$ID,`uname -m`,$ID$VERSION_ID-$MACH,$MACH;)"
+        echo HINT: name: $SHR_RUNNER_NAME - SHR_LABELS: $SHR_LABELS
+            $ASSERT ./config.sh --name "$SHR_RUNNER_NAME" --unattended --labels "$SHR_LABELS" --replace Y --url $SHR_REPO_NAME_URL --token $SHR_TOKEN
         fi
     ;;
-    (run)
-        get_SHR_REPO "$@"
-        get_SHR_NAME "$@"
+    (run) # cmd
+        get_SHR_REPO_NAME "$1"
+        get_SHR_RUNNER_NAME "$2"
         if cd $SHR_AR_DIR; then
             # Last step, run it!
-            $ASSERT ./run.sh
+            #$ASSERT ./run.sh
+            exec ./run.sh
             # Using your self-hosted runner
             # Use this YAML in your workflow file for each job
             # runs-on: self-hosted
         fi
     ;;
-    (remove)
-        get_SHR_REPO "$@"
-        get_SHR_NAME "$@"
-        if cd $SHR_AR_DIR; then
+#neville+    4657    3605  0 13:51 pts/0    00:00:00 /bin/bash /home/nevilledbld/AR/actions-runner/NevilleDNZ/algol68_autopkg/fedora-server-x86-64-39-1-5-vbox/run.sh
+#neville+    4664    4657  0 13:51 pts/0    00:00:00 /bin/bash /home/nevilledbld/AR/actions-runner/NevilleDNZ/algol68_autopkg/fedora-server-x86-64-39-1-5-vbox/run-helper.sh
+#neville+    4668    4664 19 13:51 pts/0    00:00:01 /home/nevilledbld/AR/actions-runner/NevilleDNZ/algol68_autopkg/fedora-server-x86-64-39-1-5-vbox/bin/Runner.Listener run
+    (kill) # cmd
+        get_SHR_REPO_NAME "$1"
+        get_SHR_RUNNER_NAME "$2"
+        ps -eaf | 
+            grep "/$SHR_AR_ROOT/$SHR_REPO_NAME/$SHR_RUNNER_NAME[/]" | # bin/Runner.Listener run\$" | 
+                awk '{print $2}' | xargs kill -KILL 
+    ;;
+    (remove) # cmd
+        get_SHR_REPO_NAME "$1"
+        get_SHR_RUNNER_NAME "$2"
+        if cd "$SHR_AR_DIR"; then
             if [ -f "TOKEN.txt" ]; then
                 SHR_TOKEN=`cat TOKEN.txt`
             else
-                get_TOKEN
+                get_TOKEN "$3"
             fi
-            echo SHR_TOKEN: $SHR_TOKEN
+            # echo SHR_TOKEN: $SHR_TOKEN
             $ASSERT ./config.sh remove --token $SHR_TOKEN
             cd -
-            $ASSERT mv $SHR_AR_DIR "$SHR_AR_DIR"_depr
+            #$ASSERT mv "$SHR_AR_DIR" "$SHR_AR_DIR"_depr
+            $ASSERT rm -rf "$SHR_AR_DIR"
         fi
     ;;
-    (status)
-        get_SHR_REPO "$@"
-        get_SHR_NAME "$@"
+    (status) # cmd
+        get_SHR_REPO_NAME "$1"
+        get_SHR_RUNNER_NAME "$2"
         if cd $SHR_AR_DIR; then
-            cat "TOKEN.txt"
+            echo TOKEN: `cat "TOKEN.txt"`
         fi
+        ps -eaf | 
+            grep "/$SHR_AR_ROOT/$SHR_REPO_NAME/$SHR_RUNNER_NAME[/]"
     ;;
     (*) echo Huh...;;
 esac
